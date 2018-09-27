102f356a718e6023089356eb2772997966a6101a
Sandeep Kumar <kr.sandeep.singh@gmail.com>
2018-09-27 16:43:53 +0530

    Vendor Course Model

--- MDS.Web/Models/Vendors/CourseVendor.cs
+++ MDS.Web/Models/Vendors/CourseVendor.cs
@@ -2,10 +2,36 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace MDS.Web.Models.Vendors
 {
     public class CourseVendor
     {
+        public int VendorCourseId { get; set; }
+
+        public int VendorCompanyId { get; set; }
+       
+        [Display(Name ="Course Name")]
+        public string CourseTitle { get; set; }
+
+        [Display(Name = "Search text")]
+        public string ShortDescription { get; set; }
+
+        [Display(Name = "About Course")]
+        public string LongDescription { get; set; }
+
+        [Display(Name = "Course Duration")]
+        public string Duration { get; set; }
+
+        [Display(Name = "Price")]
+        public decimal? VendorPrice { get; set; }
+
+        [Display(Name = "Course Title")]
+        public string Title { get; set; }
+
+        [Display(Name = "Course URL")]
+        public string YourUrl { get; set; }
+
     }
 }
