bf421874193b843311d2fa78fc82e5e0305bd1d8
Md Shahbaj <mdali6020@gmail.com>
2018-12-15 16:39:20 +0530

    All commite

---
+++ Testing/Controllers/SinupController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Testing.Controllers
+{
+    public class SinupController : Controller
+    {
+        // GET: Sinup
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        
+    }
+}
---
+++ Testing/Models/Sinup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Testing.Models
+{
+    public class Sinup
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string Mobile { get; set; }
+        public string Country { get; set; }
+        public string State { get; set; }
+        public string City { get; set; }
+
+    }
+}
